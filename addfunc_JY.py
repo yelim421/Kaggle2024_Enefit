094fec9c7264229d1c7ffc21f75489b69eb18c36
wnsdyd6830 <wnsdyd6830@naver.com>
2024-01-22 14:18:21 +0900

    add func from jy

--- addfunc_JY.py
+++ addfunc_JY.py
@@ -1,28 +1,33 @@
 import numpy as np #wind  
 import pandas as pd
 
-#train_dataset data 변환 (weekday -> weekend)
+#train_dataset data 변환 (weekday -> weekend, wind dir, speed -> U10, V10)
 class TrainDataTransform:
     def __init__(self, df):
         self.df = df
 
     def transform(self):
         self.is_weekend()
+        self.wind_data_to_UV()
         return self.df
 
     #weekend 판별 함수
     def is_weekend(self):
         self.df['is_weekend'] = np.where(self.df['weekday'] > 4, 1, 0)
+        
+    def wind_data_to_UV(self):
+        self.df['U10'] = self.df['windspeed_10m'] * np.cos(np.radians(270 - self.df['winddirection_10m']))
+        self.df['V10'] = self.df['windspeed_10m'] * np.sin(np.radians(270 - self.df['winddirection_10m']))
+        
 
 
-#DataStorage data 변환 (electricity price, wind)
+#DataStorage data 변환 (electricity price 음수값 양수로 변환)
 class DataStorageTransform:
     def __init__(self, df):
         self.df = df
 
     def transform(self):
         self.electricity_prices_to_positive()
-        self.wind_data_to_UV()
         return self.df
     
     #elecrricity prices 음수값 양수로 바꾸는 함수
@@ -30,10 +35,3 @@ class DataStorageTransform:
         self.df.df_electricity_prices = self.df.df_electricity_prices.with_columns(
             self.df.df_electricity_prices['euros_per_mwh'].abs().alias('euros_per_mwh'))
         
-
-    #historical wind speend & direction data to U10, V10
-    def wind_data_to_UV(self):
-        self.df.df_historical_weather = self.df.df_historical_weather.with_columns(
-            (self.df.df_historical_weather['windspeed_10m'] * np.cos(np.radians(270 - self.df.df_historical_weather['winddirection_10m']))).alias('U10'),
-            (self.df.df_historical_weather['windspeed_10m'] * np.sin(np.radians(270 - self.df.df_historical_weather['winddirection_10m']))).alias('V10')
-            )
